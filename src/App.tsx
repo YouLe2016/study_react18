3cfaff99bb2945895b0dbf16e602760aad6531de
王酉乐 <270628297@qq.com>
2024-10-14 17:12:54 +0800

    列表渲染

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 //项目的根组件
 //App --> index.js --> public/index.html(root)
+
+const list = [
+  {id:1001, name:'Vue'},
+  {id:1002, name: 'React'},
+  {id:1003, name: 'Angular'}
+]
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <h2>列表渲染</h2>
+      <ul>
+        {list.map(item =>
+          <li key={item.id}>{item.name}</li>
+        )}
+      </ul>
     </div>
   );
 }
